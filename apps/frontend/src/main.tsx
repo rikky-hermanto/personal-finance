4657e6852e78af303342d539c42fa37b6d014ed7
RIKKY-YOGA7PROX\rikky <rikky.hermanto@gmail.com>
2026-04-30 19:04:31 +0800

    RENEW! UI THEME! DARK MODE

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+document.documentElement.setAttribute('style', 'color-scheme: dark');
+
 createRoot(document.getElementById("root")!).render(<App />);
